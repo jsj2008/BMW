602caef5d623c7d66e1c967d1dc5ab7535d16e5e
Aaron Sarnoff <aaron@sarnoff.tv>
2011-06-06 02:23:41 -0700

    start cleanup process

---
+++ BMW_iOS/Classes/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  BMW_iOS
+//
+//  Created by Aaron Sarnoff on 6/6/11.
+//  Copyright 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MainViewController : UIViewController {
+    
+}
+
+@end
